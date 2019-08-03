9b1db5aeb6fc80d765e49bee11e96c0456ae7ed3
Peter <pitrens.studio@gmail.com>
2019-08-03 22:36:14 +0800

    add navigation

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "gatsby"
 
+import Footer from "../components/footer";
+import Header from "../components/header";
 
 const AboutPage = () => {
   return (
     <div>
+      <Header />
       <h1>About Page</h1>
       <p>
         In this page I will show some information so that the user can get to
         know me as a software engineer
       </p>
       <Link to="/contact">Contact Me</Link>
+      <Footer />
     </div>
   );
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,22 @@
 import React from "react"
 
+import Footer from "../components/footer";
+import Header from "../components/header";
+
 const ContactPage = () => {
   return (
     <div>
+      <Header />
       <h1>Contact</h1>
       <p>From this page you will get links to my social media profile</p>
-      <a href="https://www.linkedin.com/in/supertrens/" target="_blank">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/supertrens/" target="_blank">
+        LinkedIn
+      </a>
       <br></br>
-      <a href="https://www.facebook.com/SuperTrens" target="_blank">Facebook</a>
+      <a href="https://www.facebook.com/SuperTrens" target="_blank">
+        Facebook
+      </a>
+      <Footer />
     </div>
   )
 }
